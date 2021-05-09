5ed571e7ced40f667a2fdda7774e025949026516
Jeongjin Oh <33178048+ojj1123@users.noreply.github.com>
2021-05-10 01:53:06 +0900

    DP

--- S3/2579_계단 오르기.cpp
+++ S3/2579_계단 오르기.cpp
@@ -2,63 +2,19 @@
 using namespace std;
 #define FAST ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 
-int stair[301] = { 0, };
-int dp[301][3] = { 0, };
+int dp[302];
 int main() {
 	FAST;
 	int n;
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		cin >> stair[i];
+	vector<int> arr(n + 1, 0);
+	for (int i = 1; i <= n; i++) cin >> arr[i];
+	dp[1] = arr[1];
+	dp[2] = arr[1] + arr[2];
+	int res = 0;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], dp[i - 2] + arr[i]);
 	}
-
-	dp[n][1] = stair[n];
-	dp[n][2] = stair[n];
-	dp[n][0] = -2147483647;
-
-
-	for (int i = n - 1; i >= 1; i--) {
-		for (int j = 0; j <= 2; j++) {
-			if (j == 2) {
-				dp[i][j] = dp[i + 1][1] + stair[i];
-			}
-			if (j == 1) {
-				dp[i][j] = dp[i + 1][0] + stair[i];
-			}
-			if (j == 0) {
-				int max = 0;
-				if (max < dp[i + 1][1]) {
-					max = dp[i + 1][1];
-				}
-				if (max < dp[i + 1][2]) {
-					max = dp[i + 1][2];
-				}
-				dp[i][j] = max;
-			}
-		}
-	}
-
-
-	int ans = 0;
-	if (ans < dp[1][1]) {
-		ans = dp[1][1];
-	}
-	if (ans < dp[1][0]) {
-		ans = dp[1][0];
-	}
-	if (ans < dp[1][2]) {
-		ans = dp[1][2];
-	}
-	if (ans < dp[2][1]) {
-		ans = dp[2][1];
-	}
-	if (ans < dp[2][2]) {
-		ans = dp[2][2];
-	}
-	if (ans < dp[2][0]) {
-		ans = dp[2][0];
-	}
-
-	cout << ans;
+	cout << dp[n];
 	return 0;
 }
