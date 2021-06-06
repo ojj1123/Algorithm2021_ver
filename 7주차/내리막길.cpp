3465c468edf016ee37826e0bebdffdb22b4870df
Jeongjin Oh <33178048+ojj1123@users.noreply.github.com>
2021-06-06 11:24:53 +0900

    DP + DFS

--- 7주차/내리막길.cpp
+++ 7주차/내리막길.cpp
@@ -1,39 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-int dp[502][502];
 int board[502][502];
+int dp[502][502];
 int dx[] = { 1,0,-1,0 };
 int dy[] = { 0,1,0,-1 };
-void bfs() {
-	queue<pair<int, int>> q;
-	q.push({ 0, 0 });
-	dp[0][0] = 1;
-	while (!q.empty()) {
-		auto cur = q.front(); q.pop();
-		int x = cur.first, y = cur.second;
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+int n, m;
+int dfs(int x, int y) {
+	if (x == n - 1 && y == m - 1) return 1;
+	else if (dp[x][y] == -1) {
+		dp[x][y] = 0;
+		for (int d = 0; d < 4; d++) {
+			int nx = x + dx[d];
+			int ny = y + dy[d];
 			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (board[x][y] > board[nx][ny]) { // 내리막길이면
-				if (dp[nx][ny] == 0) dp[nx][ny] = 1;
-				else if (dp[nx][ny] <= dp[x][y]) dp[nx][ny]++;
-				q.push({ nx,ny }); // 중복해서 들르면 안됌 시간초과 남
-			}
+			if (board[x][y] > board[nx][ny]) dp[x][y] += dfs(nx, ny);
 		}
 	}
+	return dp[x][y];
 }
 int main() {
 	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 	cin >> n >> m;
+	for (int i = 0; i < n; i++) fill(dp[i], dp[i] + m + 1, -1);
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++) cin >> board[i][j];
-	bfs();
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) cout << dp[i][j] << ' ';
-		cout << '\n';
-	}
-	cout << dp[n - 1][m - 1];
+	cout << dfs(0, 0);
 }
